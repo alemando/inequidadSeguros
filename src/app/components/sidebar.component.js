c86eac5248a8aef276c169fe49ea2cfffef4ebd1
StuarTz3 <55957047+StuarTz3@users.noreply.github.com>
2019-11-15 23:59:32 -0500

    arreglos esteticos

--- src/app/components/sidebar.component.js
+++ src/app/components/sidebar.component.js
@@ -5,32 +5,33 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
-import MediaQuery from 'react-responsive' 
+import MediaQuery from 'react-responsive';
+import { Z_FIXED } from 'zlib';
 
 export default props => {
   return (
     <MediaQuery minDeviceWidth={1224} device={{ deviceWidth: 1600 }}>
-    <Menu>
+    <Menu position={Z_FIXED}>
     
-
+    
     <a className="menu-item" href="/index">
-      <p> <font color="#1565C0"> <b><h2><i className="fa fa-fw fa-home"></i> Menú </h2></b></font></p>
+       <font color="#1565C0"> <b><h2><i className="fa fa-fw fa-home"></i> Menú </h2></b></font>
       </a>
-
+      
       <a className="menu-item" href="/seguros">
-      <p><font color="#1565C0"><b><h3><i className="fa fa-shield"></i> Seguros </h3></b></font></p>
+      <font color="#1565C0"><b><h2><i className="fa fa-shield"></i> Seguros </h2></b></font>
       </a>
 
       <a className="menu-item" href="/categorias">
-      <p><font color="#1565C0"><b><h3><i className="fa fa-archive"></i> Categorías</h3></b></font></p>
+      <font color="#1565C0"><b><h2><i className="fa fa-archive"></i> Categorías</h2></b></font>
       </a>
 
       <a className="menu-item" href="/clientes">
-      <p><font color="#1565C0"><b><h3><i className="fa fa-users"></i> Clientes</h3></b></font></p>
+      <font color="#1565C0"><b><h2><i className="fa fa-users"></i> Clientes</h2></b></font>
       </a>
 
       <a className="menu-item" href="/vendedores">
-      <p><font color="#1565C0"><b><h3><i className="fa fa-id-card-o"></i> Vendedores</h3></b></font></p>
+     <font color="#1565C0"><b><h2><i className="fa fa-id-card-o"></i> Vendedores</h2></b></font>
       </a>
 
       
