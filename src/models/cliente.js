782180f6a049d4f7c414ce0d633185f82f57181c
Ricardo José Garzón Arias <42324647+Glifos6089@users.noreply.github.com>
2019-11-15 20:01:36 -0500

    validaciones de cliente realizadas

--- src/models/cliente.js
+++ src/models/cliente.js
@@ -71,26 +71,104 @@ const patronCorreo = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+
 clienteSchema.statics.guardarCliente = async (datos)=> {
     
     let validacion = { id: "0", mensaje: ""}
+    //Validacion del documento si  es un numero
+    if(isNaN(datos.documento)){
+        validacion.mensaje += "El documento no es un numero\n"
+    }
 
+    //Validacion de nularidad documento o string vacio(obligatorio)
+    if(datos.documento == null){
+        validacion.mensaje +="Documento no guardado, no puedes dejar el documento null"
+      }else if (datos.documento == "") {
+        validacion.mensaje +="Documento no guardado, asegúrese de que el documento si este ingresado"
+      }
+
+    //Validacion de nularidad nombre o string vacio(obligatorio)
+    if(datos.nombre == null){
+        validacion.mensaje +="Nombre no guardado, no puedes dejar el Nombre null"
+      }else if (datos.nombre == "") {
+        validacion.mensaje +="Nombre no guardado, asegúrese de que el Nombre si este ingresado"
+      }
+    
+    //Validacion de nularidad apellido1 o string vacio(obligatorio)
+    if(datos.apellido1 == null){
+        validacion.mensaje +="Apellido1 no guardado, no puedes dejar el Apellido1 null"
+      }else if (datos.apellido1 == "") {
+        validacion.mensaje +="Apellido1 no guardado, asegúrese de que el Apellido1 si este ingresado"
+      }
+    
+      //Validacion de nularidad apellido2
+    if(datos.apellido2 == null){
+        validacion.mensaje +="Apellido2 no guardado, no puedes dejar el Apellido2 null"
+      }
+    
+      //Validacion de nularidad direccion o string vacio(obligatorio)
+    if(datos.direcciom == null){
+        validacion.mensaje +="Direccion no guardada no puedes dejar la Direccion null"
+      }else if (datos.direccion == "") {
+        validacion.mensaje +="Direccion no guardada, asegúrese de que la Direccion si este ingresado"
+      }
+
+    //Validacion del telefono si  es un numero
+    if(isNaN(datos.telefono)){
+        validacion.mensaje += "El telefono no es un numero\n"
+    }
+
+      //Validacion de nularidad telefono o string vacio(obligatorio)
+    if(datos.telefono == null){
+        validacion.mensaje +="Telefono no guardado, no puedes dejar el Telefono null"
+      }else if (datos.telefono == "") {
+        validacion.mensaje +="Telefono no guardado, asegúrese de que el Telefono si este ingresado"
+      }
+     
     //Validacion basada en regex de el formato de un correo
     if(!patronCorreo.test(datos.correo)){
         validacion.mensaje += "El correo no sigue el formato example@dominio.ext\n"
     }
+
+      //Validacion de nularidad direccion o string vacio(obligatorio)
+    if(datos.direccion == null){
+        validacion.mensaje +="Direccion no guardada, no puedes dejar la Direccion null"
+      }else if (datos.direccion == "") {
+        validacion.mensaje +="Direccion no guardada, asegúrese de que la Direccion si este ingresado"
+      }
+     
     
     //Validacion fechaNacimiento es una fecha valida
     if(isNaN(Date.parse(datos.fechaNacimiento))){
         validacion.mensaje += "La fecha de nacimiento tiene un formato erroneo\n"
     }
+
+      //Validacion de nularidad fechaNacimiento string vacio(obligatorio)
+      if(datos.fechaNacimiento == null){
+        validacion.mensaje +="FechaNacimiento no guardado, no puedes dejar la fechaNacimiento null"
+      }else if (datos.fechaNacimiento == "") {
+        validacion.mensaje +="FechaNacimiento no guardado, asegúrese de que la fechaNacimiento si este ingresado"
+      }
     
     //Validacion ingresos es un numero 
     if(isNaN(datos.ingresos)){
         validacion.mensaje += "Los ingresos no son numeros\n"
     }
+
+      //Validacion de nularidad ingresos o string vacio(obligatorio)
+      if(datos.ingresos == null){
+        validacion.mensaje +="Ingresos no guardados, no puedes dejar los Ingresos null"
+      }else if (datos.ingresos == "") {
+        validacion.mensaje +="Ingresos no guardados, asegúrese de que los Ingresos si estan ingresados"
+      }
     
     //Validacion egresos es un numero 
     if(isNaN(datos.egresos)){
 		validacion.mensaje += "Los egresos no son numeros\n"
-	}
+    }
+    
+      //Validacion de nularidad egresos o string vacio(obligatorio)
+      if(datos.egresos == null){
+        validacion.mensaje +="egresos no guardados, no puedes dejar los egresos null"
+      }else if (datos.egresos == "") {
+        validacion.mensaje +="egresos no guardado, asegúrese de que los egresos si estan ingresados"
+      }
 
     //Si no pasa alguna validacion retorna el mensaje correspondiente
     if(validacion.mensaje.length!=0) return validacion
