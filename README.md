8628da4f32d3827fbff7f0066d936df1aed5d370
Ricardo José Garzón Arias <42324647+Glifos6089@users.noreply.github.com>
2019-11-15 19:28:40 -0500

    organizando la rama

--- README.md
+++
@@ -1,2 +0,0 @@
-# inequidadSeguros
-Proyecto de requisitos
--- src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/*router.get('/', async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
-});
-*/
-module.exports = router;
