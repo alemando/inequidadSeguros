f37fbe08c148c738ab30de68939064249dba5bf3
Santiago Espinosa <sespinosarte99@gmail.com>
2020-02-20 07:35:53 -0500

    Cambios sustanciales
    Cambiamos la librería a bcrypt, luego eliminamos crypto js

--- src/models/vendedor.js
+++ src/models/vendedor.js
@@ -1,46 +1,46 @@
 const mongoose = require('mongoose');
-const CryptoJS  = require('crypto-js');
+const bcrypt = require('bcrypt');
 var uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 
 //Clase vendedor
 const vendedorSchema = Schema({
-    documento:{
+    documento: {
         type: String,
         require: true,
         trim: true,
         unique: true
     },
-    nombre:{
+    nombre: {
         type: String,
         require: true,
         trim: true
     },
-    apellido1:{
+    apellido1: {
         type: String,
         require: true,
         trim: true
     },
-    apellido2:{
+    apellido2: {
         type: String,
         trim: true
     },
-    telefono:{
+    telefono: {
         type: Number,
         require: true,
         trim: true
     },
-    correo:{
+    correo: {
         type: String,
         require: true,
         trim: true
     },
-    contrasena:{
+    contrasena: {
         type: String,
         require: true,
-        trim: true        
+        trim: true
     },
-    esAdmin:{
+    esAdmin: {
         type: Boolean,
         require: true,
         default: false
@@ -57,29 +57,29 @@ const patronCorreo = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+
     recibe un arreglo json de parametros
     retorna un arreglo JSON {id: #, mensaje:...}
 */
-vendedorSchema.statics.guardarVendedor = async (datos)=> {
+vendedorSchema.statics.guardarVendedor = async (datos) => {
 
-    let validacion = { id: "0", mensaje: ""}
-    
+    let validacion = { id: "0", mensaje: "" }
+
     //Validacion basada en regex de el formato de un correo
-    if(!patronCorreo.test(datos.correo)){
+    if (!patronCorreo.test(datos.correo)) {
         validacion.mensaje += "El correo no sigue el formato example@dominio.ext\n"
     }
-    
+
     //Validaciones documento negativo o nulo
-    if(datos.documento == "" || datos.documento == null || parseInt(datos.documento) < 0){
+    if (datos.documento == "" || datos.documento == null || parseInt(datos.documento) < 0) {
         validacion.mensaje += "El documento de identificacion no es valido\n";
     }
-    
+
     //Si no pasa alguna validacion retorna el mensaje correspondiente
-    if(validacion.mensaje.length!=0) return validacion
+    if (validacion.mensaje.length != 0) return validacion
 
     //Encripto la contraseña mandada desde la petición
     let password = encriptar(datos.contrasena);
 
     //esAdmin valida si la sesión ha sido abierta por un admin
     //admin valida si se desea crear un admin o no
-    if(datos.esAdmin && datos.admin){
+    if (datos.esAdmin && datos.admin) {
         //Objeto vendedor
         var vendedorNuevo = new vendedores({
             documento: datos.documento,
@@ -90,9 +90,9 @@ vendedorSchema.statics.guardarVendedor = async (datos)=> {
             correo: datos.correo,
             contrasena: password,
             esAdmin: true
-            });
+        });
     }
-    else{
+    else {
         //Objeto vendedor
         var vendedorNuevo = new vendedores({
             documento: datos.documento,
@@ -102,64 +102,57 @@ vendedorSchema.statics.guardarVendedor = async (datos)=> {
             telefono: datos.telefono,
             correo: datos.correo,
             contrasena: password
-            });
+        });
     }
-    
+
     try {
         await vendedorNuevo.save();
-        return { id: "1", mensaje: "Vendedor guardado"};
+        return { id: "1", mensaje: "Vendedor guardado" };
     } catch (error) {
-        if (error.errors.documento.kind==="unique") return { 
-            id: "2", mensaje: "El documento ingresado ya existe en nuestra base de datos"};
-        else return { id: "0", mensaje: "Error desconocido"};
+        if (error.errors.documento.kind === "unique") return {
+            id: "2", mensaje: "El documento ingresado ya existe en nuestra base de datos"
+        };
+        else return { id: "0", mensaje: "Error desconocido" };
     }
 }
 
 //Obtener todos los vendedores
-vendedorSchema.statics.obtenerVendedores = async ()=> {
-    try{
+vendedorSchema.statics.obtenerVendedores = async () => {
+    try {
         let listaVendedores = await vendedores.find();
-        listaVendedores.forEach(x => x.contrasena = desencriptar(x.contrasena));
         return listaVendedores;
-    }catch(error){
+    } catch (error) {
         return "Error obteniendo los vendedores\n" + error;
     }
 }
 
 //Obtiene vendedor por el documento
-vendedorSchema.statics.obtenerVendedor = async (id)=> {
-    try{
-        let vendedor = await vendedores.findOne({documento: id});
-        vendedor.contrasena = desencriptar(vendedor.contrasena);
+vendedorSchema.statics.obtenerVendedor = async (id) => {
+    try {
+        let vendedor = await vendedores.findOne({ documento: id });
         return vendedor;
-    }catch(error){
+    } catch (error) {
         return "Error obteniendo vendedor por documento identidad\n" + error;
     }
 }
 
 //Obtiene vendedor por el id
-vendedorSchema.statics.obtenerVendedorById = async (id)=> {
-    try{
+vendedorSchema.statics.obtenerVendedorById = async (id) => {
+    try {
         let vendedor = await vendedores.findById(id);
         return vendedor;
-    }catch(error){
+    } catch (error) {
         return "Error obteniendo vendedor por documento identidad\n" + error;
     }
 }
 
 //Funcion para encriptar contraseñas
-function encriptar(password){
-    return CryptoJS.AES.encrypt(password, 'key').toString();
+function encriptar(password) {
+    // return CryptoJS.AES.encrypt(password, 'key').toString();
+    return bcrypt.hashSync(password, 10).toString();
 }
-
-//Funcion para desencriptar los datos
-function desencriptar(textoCifrado){
-    let bytes = CryptoJS.AES.decrypt(textoCifrado, 'key');
-    return bytes.toString(CryptoJS.enc.Utf8);
-}
-
 
 //Se retorna clase vendedores para exportar
-const vendedores = mongoose.model('vendedores',vendedorSchema);
+const vendedores = mongoose.model('vendedores', vendedorSchema);
 
 module.exports = vendedores;
