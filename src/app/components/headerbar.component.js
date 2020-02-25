93bd1eec7459bedb4fda413c387c3aae1380b024
salitomeyo <53347835+salitomeyo@users.noreply.github.com>
2020-02-25 16:11:03 -0500

    headerbar
    commit

--- src/app/components/headerbar.component.js
+++ src/app/components/headerbar.component.js
@@ -6,10 +6,22 @@ export default class headerbar extends Component {
   render(){
     return (
       <div className="headerbar">
+        <div className="headerbar-left">
+          <a href="/index" className="logo"><img alt="logo" src="assets/images/logo.png" /> <span>Seguros</span></a>
+          <a href="/index" className="logo"><img alt="logo" src="assets/images/logo.png" /> <span>Seguros</span></a>
+        </div>
 
-              <div className="headerbar-left">
-            <a href="/index" className="logo"><img alt="logo" src="assets/images/logo.png" /> <span>Seguros</span></a>
-              </div>
+              
+        <nav className="navbar-custom">
+          <ul className="list-inline menu-left mb-0">
+            <li className="float-left">
+              <button className="button-menu-mobile open-left">
+                  <i className="fa fa-fw fa-bars"></i>
+              </button>
+            </li>                        
+          </ul>
+        </nav>
+              
 
               <nav className="navbar-custom">
 
