6f8a16a169513f520e0e3b6014a1d31bbe3f8ca7
Alejandro <35750771+alemando@users.noreply.github.com>
2020-02-27 01:18:39 -0500

    cambios admin

--- src/routes/seguro.routes.js
+++ src/routes/seguro.routes.js
@@ -29,7 +29,7 @@ router.get('/principal/:id', async(req,res)=>{
 });
 
 router.post('/finiquitar/:id', async(req,res)=>{
-  let respuesta = await Seguro.cambiarEstado(req.params.id,req.body.estado,req.body.admin)
+  let respuesta = await Seguro.cambiarEstado(req.params.id,req.body.estado, req.session.esAdmin)
   res.json(respuesta);
 })
 //Borrar seguro por Id de seguro
