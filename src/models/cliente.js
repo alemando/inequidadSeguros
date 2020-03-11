438f83a2cb6742a1b3e5f2f6b02e6740c8e1073b
oyeu <daarangovi@gmail.com>
2020-03-10 20:00:40 -0500

    Metodo para generar top cinco de clientes completado.

--- src/models/cliente.js
+++ src/models/cliente.js
@@ -449,15 +449,31 @@ clienteSchema.statics.cambiarEstadoCliente = async (documento, admin) => {
 }
 
 clienteSchema.statics.topCinco = async () => {
-    let top = []
-    lista_clientes = await clientes.find({},["_id","documento","nombre","apellido1","apellido2"])
-    for (const cli of lista_clientes) {  
-        numero_seguros = await seguroModel.countDocuments({cliente:cli._id})
-        top.push({"id":cli._id})
-        } 
-    console.log(top);
-    
-    return lista_clientes
+    let top = [];
+    try {
+        lista_clientes = await clientes.find({},["_id","documento","nombre","apellido1","apellido2"]);
+        if(lista_clientes.length==0) return {id:0, mensaje:"No hay clientes registrados!"}
+        for (const cli of lista_clientes) {  
+            numero_seguros = await seguroModel.countDocuments({cliente:cli._id,estado:"Aprobado"});
+            if(numero_seguros!=0) top.push({id:cli._id,documento:cli.documento,nombres:cli.nombre,apellidos: cli.apellido1 + ' '+ cli.apellido2,seguros:numero_seguros});
+        }
+        if (top.length==0) return {id:0, mensaje:"No hay clientes con seguros aprobados, no se pudo generar el top"}
+        else {
+            top=top.sort((a,b)=>b.seguros-a.seguros)
+            let mensaje = "top generado con exito!."
+            if (top.length > 5) {
+                let empate = 5;
+                while (top[empate]) {
+                    if (top[empate-1].seguros == top[empate].seguros) empate = empate + 1;
+                }
+                top = top.slice(0,empate);
+                mensaje = mensaje + " se produjo un empate, se daran " + empate + " resultados"
+            }
+            return {id:1, mensaje:mensaje, top:top}    
+        }
+    } catch (error) {
+        return {id:0, mensaje:"ha ocurrido algo inesperado\n" + error}
+    }
 }
 
 const clientes = mongoose.model('clientes', clienteSchema);
