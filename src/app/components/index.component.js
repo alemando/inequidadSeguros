35ffd9a928c8ead91b0b227048019450e7956426
jsapuyesp <jsapuyesp@unal.edu.co>
2020-03-15 15:42:23 -0500

    HU Terminada

--- src/app/components/index.component.js
+++ src/app/components/index.component.js
@@ -8,18 +8,19 @@ export default class Index extends Component {
   render() {
     return (
       <div className="row">
-          <div className="col-sm-6">
+          <div className="col-sm-12">
+            {this.props.session.esAdmin ? <SegurosPendientes session={this.props.session}></SegurosPendientes> :""}
+          </div>
+          <div className="col-sm-4">
             {this.props.session.esAdmin ? <MejoresVendedores session={this.props.session}></MejoresVendedores> : ""}
           </div>
-          <div className="col-sm-6">
+          <div className="col-sm-4">
             {this.props.session.esAdmin ? <MejoresAseguradoras session={this.props.session}></MejoresAseguradoras> : ""}
           </div>
           <div className="col-sm-4">
             <CantidadClientes session={this.props.session}></CantidadClientes>
           </div>
-          <div className="col-sm-8">
-            {this.props.session.esAdmin ? <SegurosPendientes session={this.props.session}></SegurosPendientes> :""}
-          </div>
+          
       </div>
     );
   }
