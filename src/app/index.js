189cda7d7e43bdc23915d685a84f916e36c08ebb
Alejandro <35750771+alemando@users.noreply.github.com>
2020-03-16 20:27:28 -0500

    cambios

--- src/app/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Login from "./components/login.component";
-
-ReactDOM.render(<Login/>,  document.getElementById('root'));
-
--- src/routes/index.routes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const session = require('express-session');
-
-module.exports = router;
